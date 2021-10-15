b812044b1be5cece95313f85357f012c59370ddd
Kalle Tiihonen <kall1429@stud.kea.dk>
2021-10-15 12:33:26 +0200

    fixed colors on details list

--- script.js
+++ script.js
@@ -23,9 +23,9 @@ function showData(oneBike) {
     copy.querySelector("h3.name").textContent = oneBike.title.rendered;
     copy.querySelector("img").setAttribute("src", oneBike._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url);
     // copy.querySelector("img").src = " oneBike._embedded["wp:featuredmedia"][0].media_details.sizes.large.source_url";
-    copy.querySelector("li.price").textContent = "Price - " + oneBike.price;
-    copy.querySelector("li.color").textContent = "Colours - " + oneBike.colors;
-    copy.querySelector("li.stock").textContent = "In Stock - " + oneBike.in_stock;
+    copy.querySelector(".price").textContent = oneBike.price;
+    copy.querySelector(".color").textContent = oneBike.colors;
+    copy.querySelector(".stock").textContent = oneBike.in_stock;
     // oneBike.colors.forEach(color => {
     //     const li = document.createElement("li");
     //     li.style.background = color;
